02cea449424f621d8ef41f2b0038c8b13dab54ee
Kedap <kedap.dev@protonmail.com>
2023-10-16 01:22:11 -0600

    feat(tesamen): Agregando preguntas funcionales

--- tesamen/tesamen.go
+++ tesamen/tesamen.go
@@ -7,8 +7,10 @@ import (
 	"io"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
+	"github.com/nsf/termbox-go"
 )
 
 type JsonExamen struct {
@@ -39,23 +41,72 @@ func (e *Estudiante) ResponderPruebaMal() {
 type PantallaSimple struct {
 	TituloExamen string
 	Descripcion  string
-	PSiguiente   *Pantalla
-	PAnterior    *Pantalla
+	PSiguiente   Pantalla
+	PAnterior    Pantalla
 	//Conrtoles
 }
 type Pantalla interface {
 	Cabezero()
 	cuerpo()
+	ObtenerSiguiente() Pantalla
+	ObtenerAnterior() Pantalla
+	renderizarPregunta()
 }
 
-func (p *PantallaSimple) cuerpo() {}
+func (p *PantallaSimple) cuerpo()                    {}
+func (p *PantallaSimple) renderizarPregunta()        {}
+func (p *PantallaSimple) ObtenerSiguiente() Pantalla { return p.PSiguiente }
+func (p *PantallaSimple) ObtenerAnterior() Pantalla  { return p.PAnterior }
 func (p *PantallaSimple) Cabezero() {
 	fmt.Printf("\x1bc")
 	fmt.Printf("\t\t\t--==[ %s ]==--\n\n\t\t%s\n\n", p.TituloExamen, p.Descripcion)
 }
+
 func mostrarPantalla(p Pantalla) {
-	p.Cabezero()
-	p.cuerpo()
+	switch p.(type) {
+	case *PantallaIncio:
+		p.Cabezero()
+		p.cuerpo()
+		mostrarPantalla(p.ObtenerSiguiente())
+	case *PPregunta:
+		p.renderizarPregunta()
+	default:
+		err := termbox.Init()
+		if err != nil {
+			fmt.Println("Oh no, ocurrio el error", err)
+			os.Exit(1)
+		}
+		defer termbox.Close()
+		canalEventos := make(chan termbox.Event)
+		go func() {
+			for {
+				canalEventos <- termbox.PollEvent()
+			}
+		}()
+		p.Cabezero()
+		p.cuerpo()
+		for {
+			select {
+			case ev := <-canalEventos:
+				if ev.Type == termbox.EventKey {
+					switch {
+					case ev.Ch == 68:
+						anterior := p.ObtenerAnterior()
+						if anterior != nil {
+							termbox.Close()
+							mostrarPantalla(anterior)
+						}
+					case ev.Ch == 67:
+						siguiente := p.ObtenerSiguiente()
+						if siguiente != nil {
+							termbox.Close()
+							mostrarPantalla(siguiente)
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 type PantallaIncio struct {
@@ -109,20 +160,20 @@ func (p *PantallaTutorial) cuerpo() {
 
 type PPregunta struct {
 	*PantallaCompuesta
-	Pregunta          string
-	Respuestas        []string
-	RespuestaCorrecta int
-	//Controladores
-	estudiante            *Estudiante
+	Pregunta              string
+	Respuestas            []string
+	RespuestaCorrecta     int
+	estudiante            Estudiante
 	respuestaSeleccionada int
 	respuestaElegida      int
+	instrucciones         string
 }
 type Pregunta interface {
-	responder(respuesta int)
+	responder()
 }
 
-func (p *PPregunta) responder(respuesta int) {
-	if respuesta == p.RespuestaCorrecta {
+func (p *PPregunta) responder() {
+	if p.respuestaElegida == p.RespuestaCorrecta {
 		p.estudiante.ResponderBien()
 	} else {
 		p.estudiante.ResponderMal()
@@ -151,43 +202,62 @@ func (p *PPregunta) cuerpo() {
 		}
 	}
 }
+func (p *PPregunta) renderizarPregunta() {
+	p.Cabezero()
+	p.cuerpo()
+	err := termbox.Init()
+	if err != nil {
+		fmt.Println("Oh no, ocurrio el error al inicializar los controladores:", err)
+		os.Exit(1)
+	}
+	defer termbox.Close()
+	canalEventos := make(chan termbox.Event)
+	go func() {
+		for {
+			canalEventos <- termbox.PollEvent()
+		}
+	}()
+	p.Cabezero()
+	p.cuerpo()
+	fmt.Println("\n\n", p.instrucciones)
+	for {
+		select {
+		case ev := <-canalEventos:
+			if ev.Type == termbox.EventKey {
+				switch {
+				case ev.Ch == 68:
+					anterior := p.ObtenerAnterior()
+					if anterior != nil {
+						termbox.Close()
+						mostrarPantalla(anterior)
+					}
+				case ev.Ch == 67:
+					siguiente := p.ObtenerSiguiente()
+					if siguiente != nil {
+						termbox.Close()
+						mostrarPantalla(siguiente)
+					}
+				case ev.Ch == 65:
+					if p.respuestaSeleccionada-1 != -1 {
+						p.respuestaSeleccionada--
+					}
+				case ev.Ch == 66:
+					if p.respuestaSeleccionada+1 != len(p.Respuestas) {
+						p.respuestaSeleccionada++
+					}
+				case ev.Key == termbox.KeyEnter:
+					p.respuestaElegida = p.respuestaSeleccionada
+					p.responder()
+				}
+				time.Sleep(60 * time.Microsecond)
+				p.renderizarPregunta()
+			}
+		}
+	}
+}
 
 func main() {
 	daniel := Estudiante{}
-	primera := PantallaIncio{
-		PantallaSimple: &PantallaSimple{
-			TituloExamen: "Pruba en Go",
-		},
-		Estudiante: &daniel,
-	}
-	mostrarPantalla(&primera)
-	const TEXTO_TUTORIAL = `
-          <- (Flecha izquierda) Ir a la anterior
-
-          -> (Flecha derecha) Ir a la siguiente
-
-          /\ (Flecha arriba) Seleccionar arriba
-          ||
-
-          || (Flecha abajo) Seleccionar abajo
-          \/
-
-          ENTER Elegir la opción
-        `
-	instrucciones := PantallaTutorial{
-		PantallaCompuesta: &PantallaCompuesta{
-			PantallaSimple: &PantallaSimple{
-				TituloExamen: "Prueba en Go",
-				Descripcion:  "Tutorial",
-			},
-			titulo: "instrucciones",
-		},
-		instrucciones: TEXTO_TUTORIAL,
-		msgFinal:      "Las siguientes preguntas solo seran de prueba",
-	}
-	mostrarPantalla(&instrucciones)
-	fmt.Scanln()
-	fmt.Println("Leyendo examen...")
 	jsonFile, err := os.Open("examen.json")
 	if err != nil {
 		fmt.Println("Ocurrio un error al leer el examen:c")
@@ -197,26 +267,57 @@ func main() {
 	valorBytes, _ := io.ReadAll(jsonFile)
 	var examen JsonExamen
 	json.Unmarshal(valorBytes, &examen)
-	fmt.Println(examen)
-	fmt.Scanln()
-	// examen.Preguntas[0] = PPregunta{
-	// 	PantallaCompuesta: &PantallaCompuesta{
-	// 		PantallaSimple: &PantallaSimple{
-	// 			TituloExamen: "Prueba en Go",
-	// 			Descripcion:  "1 de 10",
-	// 		},
-	// 		titulo:           "¿Que dia fue el eclipse solar?",
-	// 		preguntasTotales: 10,
-	// 	},
-	// 	estudiante: &daniel,
-	// }
+	examen.Preguntas[2].PantallaCompuesta = &PantallaCompuesta{
+		PantallaSimple: &PantallaSimple{
+			TituloExamen: "Prueba en Go",
+			Descripcion:  "Prueba 1 de 2",
+			PAnterior:    &examen.Preguntas[1],
+		},
+		titulo:           "¿Que dia es hoy?",
+		preguntasTotales: 10,
+	}
+	examen.Preguntas[2].instrucciones = "\t<- Ir a la anterior  -> Ir a la siguiente\n\t/\\ Seleccionar arriba \\/ Seleccionar abajo \n\tENTER elegir opción"
 	examen.Preguntas[1].PantallaCompuesta = &PantallaCompuesta{
 		PantallaSimple: &PantallaSimple{
 			TituloExamen: "Prueba en Go",
-			Descripcion:  "1 de 10",
+			Descripcion:  "Prueba 2 de 2",
+			PSiguiente:   &examen.Preguntas[2],
 		},
 		titulo:           "¿Que dia fue el eclipse solar?",
 		preguntasTotales: 10,
 	}
-	mostrarPantalla(&examen.Preguntas[1])
+	examen.Preguntas[1].instrucciones = "\t<- Ir a la anterior  -> Ir a la siguiente\n\t/\\ Seleccionar arriba \\/ Seleccionar abajo \n\tENTER elegir opción"
+	const TEXTO_TUTORIAL = `
+          <- (Flecha izquierda) Ir a la anterior
+
+          -> (Flecha derecha) Ir a la siguiente
+
+          /\ (Flecha arriba) Seleccionar arriba
+
+
+          \/ (Flecha abajo) Seleccionar abajo
+
+
+          ENTER Elegir la opción
+        `
+	instrucciones := PantallaTutorial{
+		PantallaCompuesta: &PantallaCompuesta{
+			PantallaSimple: &PantallaSimple{
+				TituloExamen: "Prueba en Go",
+				Descripcion:  "Tutorial",
+				PSiguiente:   &examen.Preguntas[1],
+			},
+			titulo: "instrucciones",
+		},
+		instrucciones: TEXTO_TUTORIAL,
+		msgFinal:      "Las siguientes preguntas solo seran de prueba",
+	}
+	primera := PantallaIncio{
+		PantallaSimple: &PantallaSimple{
+			TituloExamen: "Pruba en Go",
+			PSiguiente:   &instrucciones,
+		},
+		Estudiante: &daniel,
+	}
+	mostrarPantalla(&primera)
 }
