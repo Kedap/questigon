bf16fb8c288a54eff5281d119ba865c2b5d4b0d3
Kedap <kedap.dev@protonmail.com>
2023-10-29 13:38:47 -0600

    feat(tesamen/compilar.bat): Se compila sin inf de compilación

--- tesamen/compilar.bat
+++ tesamen/compilar.bat
@@ -1,3 +1,3 @@
 go version
-go build tesamen.go estudiante.go examen.go pantalla_calf.go pantalla_compuesta.go pantalla_confir.go pantalla_inicio.go pantalla_pregunta.go pantalla_simple.go pantalla_tuto.go pantallas_tuto.go
+go build build -ldflags "-s -w" tesamen.go estudiante.go examen.go pantalla_calf.go pantalla_compuesta.go pantalla_confir.go pantalla_inicio.go pantalla_pregunta.go pantalla_simple.go pantalla_tuto.go pantallas_tuto.go
 PAUSE
