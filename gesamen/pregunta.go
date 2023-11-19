4cfbc826455a382336d483aecf66341b2c7b712c
Kedap <kedap.dev@protonmail.com>
2023-11-19 16:14:31 -0600

    doc(pregunta): comentando metodo `aContenedor`

--- gesamen/pregunta.go
+++ gesamen/pregunta.go
@@ -24,9 +24,21 @@ func (p *Pregunta) Responder(respuesta string) {
 	}
 }
 
-func (p *Pregunta) a_contenedor() *fyne.Container {
+// aContenedor method : Retorna un contenedor con el nombre de la pregunta
+// opciones y una función que la resuelve `p.Responder(respuestaElegida)`
+func (p *Pregunta) aContenedor() *fyne.Container {
 	contenedorPregunta := container.NewVBox(widget.NewLabel(p.Pregunta))
 	opciones := widget.NewRadioGroup(p.Respuestas, func(r string) { p.Responder(r) })
 	contenedorPregunta.Add(opciones)
 	return contenedorPregunta
 }
+
+//
+// func agregarPregAVent(preguntas []Pregunta, w fyne.Window) {
+// 	cuestionario := container.NewVBox()
+// 	for _, pregunta := range preguntas {
+// 		contenedorPregunta := pregunta.aContenedor()
+// 		cuestionario.Add(contenedorPregunta)
+// 	}
+// 	w.SetContent(container.NewVScroll(cuestionario))
+// }
