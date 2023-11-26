866f5e89045aa8849766f55c86dca3e29579f8c1
Kedap <kedap.dev@protonmail.com>
2023-11-25 23:51:08 -0600

    test(gesamen): agregando un nuevo test para la doble respesta

--- gesamen/pregunta_test.go
+++ gesamen/pregunta_test.go
@@ -22,6 +22,27 @@ func TestPreguntaResponder(t *testing.T) {
 	}
 }
 
+func TestPreguntaResponderDoble(t *testing.T) {
+	pregunta := Pregunta{
+		Pregunta:          "¿Cual es el dia de la independencia?",
+		Respuestas:        []string{"11/11", "25/12", "16/9"},
+		RespuestaCorrecta: 3,
+		estudiante:        &Estudiante{Nombre: "Test"},
+	}
+
+	if pregunta.resuelta {
+		t.Error("FALLO: La pregunta no puede estar resuelta por defecto")
+	}
+
+	const RESPUESTA_CORRECTA = "16/9"
+	pregunta.Responder(RESPUESTA_CORRECTA)
+	pregunta.Responder(RESPUESTA_CORRECTA)
+
+	if !pregunta.resuelta {
+		t.Error("FALLO: El metodo responder no funciona correctamente")
+	}
+}
+
 func TestPreguntaResponderEstudiante(t *testing.T) {
 	estudiante := Estudiante{
 		Nombre:              "Test",
